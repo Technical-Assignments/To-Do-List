abe28502c6a904645e13a686bd7dbb9c70d3957a
ahmedibai <am.ibai.1501@gmail.com>
2020-10-18 17:11:25 +0300

    creat a todo input component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TodoInput from './Components/TodoInput';
+import TodoList from './Components/TodoList';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import uuid from 'uuid'
 
-function App() {
-  return (
-    <div>
-      Hello From App Component 
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-10 mx-auto col-md-8 mt-4'>
+            <h3 className='text-capitalize text-center'>todo input</h3>
+          <TodoInput />
+          <TodoList />
+
+          </div>
+
+        </div>
+
+      </div>
+     
+    )
+  }
 }
 
-export default App;
+export default App
+
+
+
