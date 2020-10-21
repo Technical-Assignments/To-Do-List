a0aeb1ccb191fba2e2b4b39ae4c9acdc933b456e
ahmedibai <am.ibai.1501@gmail.com>
2020-10-21 21:28:10 +0300

    modify

--- src/App.js
+++ src/App.js
@@ -2,18 +2,53 @@ import React, { Component } from 'react';
 import TodoInput from './Components/TodoInput';
 import TodoList from './Components/TodoList';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import uuid from 'uuid'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import uuid from 'uuid'
+import {v1 as uuid} from "uuid";
 
 class App extends Component {
+    state = {
+      itmes: [],
+      id: uuid(),
+      item: '',
+      editItem: false
+       
+    }
+
+    handleChange = (e) => {
+      this.setState({
+        item: e.target.value
+      });
+    };
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    };
+    console.log(newItem, "aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+    const updatedItems = [...this.state.itmes, newItem];
+
+    this.setState({
+      itmes: updatedItems,
+      item: '',
+      id: uuid(),
+      editItem: false
+    });
+
+  };
+
   render() {
     return (
       <div className='container'>
         <div className='row'>
           <div className='col-10 mx-auto col-md-8 mt-4'>
             <h3 className='text-capitalize text-center'>todo input</h3>
-          <TodoInput />
-          <TodoList />
+          <TodoInput item= {this.state.item} handleChange= {this.handleChange} handleSubmit= {this.handleSubmit} />
+          <TodoList items= {this.state.items} />
 
           </div>
 
--- src/Components/TodoInput.js
+++ src/Components/TodoInput.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
  class TodoInput extends Component {
     render() {
+        const { item, handleChange } = this.props;
         return (
             <div className='card card-body my-3'>
                 <form>
@@ -13,7 +14,9 @@ import React, { Component } from 'react'
                         </div>
                         <input type='text' 
                         className='form-control text-capitalize' 
-                        placeholder='add a todo item' />
+                        placeholder='add a todo item' 
+                        value= {item} 
+                        onChange= {handleChange} />
                     </div>
                     <button type='submit' className='btn btn-block btn-primary mt-3'>
                         add item
